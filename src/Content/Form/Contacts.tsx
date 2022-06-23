2690576d03c5814dbf8aa8ff1c4e87967615b959
Valentin-Prokofev <valentin.prokofbev@gmail.com>
2022-06-23 22:24:09 +0300

    remade Contacts component

--- src/Content/Form/Contacts.tsx
+++ src/Content/Form/Contacts.tsx
@@ -5,29 +5,35 @@ import vk_icon from "../../assets/image/social_icons/icons8-vk-com.svg";
 import github_icon from "../../assets/image/social_icons/icons8-github.svg";
 import facebook_icon from "../../assets/image/social_icons/icons8-facebook.svg";
 import instagram_icon from "../../assets/image/social_icons/icons8-instagram.svg";
+import mail from "../../assets/image/contacts/mail-svgrepo-com (1).svg";
+import phone from "../../assets/image/contacts/phone-svgrepo-com.svg";
 
 export const Contacts = () => {
     return (
-        <div className={style.form}>
+        <section className={style.form}>
             <div className={style.form_container}>
                 <HeaderSection title={"contact"} subtitle={"Get in Touch"}/>
                 <div className={style.form_items}>
                     <div className={style.form_info}>
                         <h2>address</h2>
-                        <p>
+                        <p className={style.form_info_p_address}>
                             <span>Red Square 1</span>
                             <span>Moscow</span>
                             <span>Russia</span>
                         </p>
                         <p>
-                            <span>7(925) 223 65 54</span>
+                            <span>
+                                <img className={style.phone_svg} src={phone} alt="tel"/>
+                                7(925) 223 65 54</span>
                         </p>
                         <p>
-                            <span>valentin.prokofbev@gmail.com</span>
+                            <span className={style.form_info_email}>
+                                <img className={style.mail_svg} src={mail} alt="mail"/>
+                                valentin.prokofbev@gmail.com</span>
                         </p>
                         <h2>follow</h2>
                         <ul className={style.follow}>
-                            <li><a href="#"><img src={vk_icon} alt="vk"/></a></li>
+                            <li><a className={style.vk_icon} href="#"><img src={vk_icon} alt="vk"/></a></li>
                             <li><a href="#"><img src={github_icon} alt="github"/></a></li>
                             <li><a href="#"><img src={facebook_icon} alt="facebook"/></a></li>
                             <li><a href="#"><img src={instagram_icon} alt="instagram"/></a></li>
@@ -37,8 +43,12 @@ export const Contacts = () => {
                         <h2 className={style.forms_title}>send us a note</h2>
                         <form className={style.forms_main}>
                             <div className={style.forms_flex}>
-                                <div className={style.form_name_input}><input className={style.form_control} name={"Name"} type="text" placeholder={"Name"}/></div>
-                                <div className={style.form_email_input}><input className={style.form_control} name={"Email"} type="email" placeholder={"Email"}/></div>
+                                <div className={style.form_name_input}><input className={style.form_control}
+                                                                              name={"Name"} type="text"
+                                                                              placeholder={"Name"}/></div>
+                                <div className={style.form_email_input}><input className={style.form_control}
+                                                                               name={"Email"} type="email"
+                                                                               placeholder={"Email"}/></div>
                                 <div className={style.form_textarea_input}>
                                     <textarea
                                         className={style.form_control}
@@ -46,14 +56,14 @@ export const Contacts = () => {
                                         placeholder={"Tell us more about your needs........"}/>
                                 </div>
                             </div>
-                            <p>
-                                <button>Send Message</button>
+                            <p className={style.form_p_btn}>
+                                <button className={style.form_btn}>Send Message</button>
                             </p>
                         </form>
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
     );
 };
 
